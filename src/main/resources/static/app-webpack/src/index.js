dd624d8de8eb9b5a340b81da5b929fd05001e84b
Vasyl Savitskyy <vasylsavitskyy@gmail.com>
2020-02-17 17:09:50 +0200

    https://coursehunter.net/course/osnovy-webpack-4

--- src/main/resources/static/app-webpack/src/index.js
+++ src/main/resources/static/app-webpack/src/index.js
@@ -1,3 +1,21 @@
-import bar from './bar';
+import nav from "./nav";
+import makeButton from "./button";
+import {makeColorStyle} from "./button-styles";
+import imageUrl from "./icon-square-big.jpg";
+import makeImage from "./image";
+import footerStyles from "./footer.css";
+import buttonStyles from "./button.css";
+const loadFooter = () => import("./footer");
 
-bar();
+const image = makeImage(imageUrl);
+const button = makeButton("Yay! A Button!");
+
+button.addEventListener("click", e => {
+    loadFooter().then(m => {
+        document.body.appendChild(m.footer);
+    });
+});
+
+button.style = makeColorStyle("cyan");
+document.body.appendChild(button);
+document.body.appendChild(image);
